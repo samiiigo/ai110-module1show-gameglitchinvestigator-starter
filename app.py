5b334c1be4a946fb5bb7f40643b23ba4116128af
Samyog Shrestha <184324726+samiiigo@users.noreply.github.com>
2026-03-04 21:48:57 -0600

    Hard difficulty range is wrong, changed it to 1-200

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def get_range_for_difficulty(difficulty: str):
     if difficulty == "Normal":
         return 1, 100
     if difficulty == "Hard":
-        return 1, 50
+        return 1, 200
     return 1, 100
 
 
